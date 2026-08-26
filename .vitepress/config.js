7446d14f8973e6e0cf9be2c014634b94028553b2
Zoca Ming <94791135+Zocamingg@users.noreply.github.com>
2026-08-26 13:08:38 +0300

    Add VitePress configuration for Zoca Network Wiki

---
+++ .vitepress/config.js
@@ -0,0 +1,38 @@
+export default {
+  title: "Zoca Network Wiki",
+  themeConfig: {
+    sidebar: [
+      { text: '👋 Hoş Geldin!', link: '/' },
+      {
+        text: 'GİRİŞ',
+        items: [
+          { text: '🖥️ Bilgisayar (Java)', link: '/giris/bilgisayar-java' },
+          { text: '📱 Mobil (Bedrock)', link: '/giris/mobil-bedrock' },
+          { text: '🔑 Giriş-Kayıt', link: '/giris/giris-kayit' },
+          { text: '🔒 İki Aşamalı Doğrulama', link: '/giris/iki-asamali-dogrulama' },
+          { text: '🛡️ Hesap Eşleşme', link: '/giris/hesap-eslesme' }
+        ]
+      },
+      {
+        text: 'KURALLAR',
+        items: [
+          { text: '📓 Genel Kurallar', link: '/kurallar/genel-kurallar' },
+          { text: '🔇 Mute Süreleri', link: '/kurallar/mute-sureleri' },
+          { text: '🚫 Yasaklanma Süreleri', link: '/kurallar/yasaklanma-sureleri' },
+          { text: '💬 Discord Kuralları', link: '/kurallar/discord-kurallari' }
+        ]
+      },
+      {
+        text: 'TOWNY',
+        items: [
+          { text: '🛖 Towny Nedir', link: '/towny/towny-nedir' },
+          { text: '❔ Towny Nasıl Oynanır', link: '/towny/towny-nasil-oynanir' },
+          { text: '📈 Oyun Komutları', link: '/towny/oyun-komutlari' },
+          { text: '🔒 Kilit Sistemi', link: '/towny/kilit-sistemi' },
+          { text: '🏹 Kuşatma Savaşı Nedir?', link: '/towny/kusatma-savasi-nedir' },
+          { text: '🎯 Trap Sistemi Nedir?', link: '/towny/trap-sistemi-nedir' }
+        ]
+      }
+    ]
+  }
+}
